dd632d1ad5015257cdc5072b358cfbe6e9fa19d7
Cookies-Yan <williamcgyn@gmail.com>
2021-04-28 14:03:04 -0500

    ok

---
+++ src/test/java/com/yan/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
